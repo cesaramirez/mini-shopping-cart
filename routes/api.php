69e43a4c66dc81b4ade953c2871207d97ff11d75
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-04-29 09:00:43 -0600

    :sparkles: :lock: Add auth methods with jwt

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+$router->namespace('API\V1\Auth')
+       ->prefix('v1/auth')
+       ->middleware('guest:api')
+       ->group(function ($router) {
+           $router->group(['middleware' => 'guest:api'], function ($router) {
+               $router->post('login', 'LoginController@login');
+               $router->post('register', 'RegisterController@register');
+               $router->post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
+               $router->post('password/reset', 'ResetPasswordController@reset');
+           });
+           $router->group(['middleware' => 'auth:api'], function ($router) {
+               $router->post('logout', 'LoginController@logout');
+               $router->post('me', 'LoginController@me');
+           });
+       });
